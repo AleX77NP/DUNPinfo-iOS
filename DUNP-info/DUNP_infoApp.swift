9efee0b7c523c86fbb9b3800611500fce2fc2a3a
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2020-11-04 19:35:25 +0100

    Initial Commit

---
+++ DUNP-info/DUNP_infoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DUNP_infoApp.swift
+//  DUNP-info
+//
+//  Created by Aleksandar on 04/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct DUNP_infoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
